713e56896c1725e5a055d41b3f4b01dcec70c35c
Tuna <tunakrdnz@gmail.com>
2023-03-21 13:53:08 +0300

    made promise chain

---
+++ Promises-Lucky-Draw/promisesChain.js
@@ -0,0 +1,21 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+
+  const players = ['Joe', 'Sabrina', 'Caroline']
+
+  players.forEach(player => {
+    luckyDraw(player)
+      .then(resolve => console.log(resolve))
+      .catch(error => console.error(error))
+  })
